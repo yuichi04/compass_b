5f9c5d58356bfc2e8c6cb8760f81b047243672b2
yuichi04 <sparklingengineer.yuya04gen@gmail.com>
2022-06-12 07:34:59 +0900

    Add credentials: true to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins "https://d26naxoetp9qtj.cloudfront.net/"
     origins ENV['CLIENT_URL']
 
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*',
+        headers: :any,
+
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+
+        credentials: true
   end
+end
