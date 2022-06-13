9f02ead4d5e1db2d033812d2f709014ebbeb6647
yuichi04 <sparklingengineer.yuya04gen@gmail.com>
2022-06-13 10:00:01 +0900

    Fix service key

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -17,7 +17,8 @@ class Api::V1::RegistrationsController < ApplicationController
       }
 
       if Rails.env.production?
-        str = ENV['SERVICE_KEY'].gsub(/\\n/, "\n")
+        tmp = ENV['SERVICE_KEY_F'] + ENV['SERVICE_KEY_L']
+        str = tmp.gsub(/\\n/, "\n")
         rsa_private = OpenSSL::PKey::RSA.new(str);
       elsif  Rails.env.development?
         rsa_private = OpenSSL::PKey::RSA.new(File.read(Rails.root.join('auth/service.key')))
