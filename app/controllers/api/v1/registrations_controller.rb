e37e87277da88ee4e8ae7bc381892d9484394555
yuichi04 <sparklingengineer.yuya04gen@gmail.com>
2022-06-13 15:52:37 +0900

    Change mailer url for test

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -16,18 +16,20 @@ class Api::V1::RegistrationsController < ApplicationController
           exp: ( Time.now + 30.minutes ).to_i
       }
 
-      if Rails.env.production?
-        tmp = ENV['SERVICE_KEY_F'] + ENV['SERVICE_KEY_L']
-        str = tmp.gsub(/\\n/, "\n")
-        rsa_private = OpenSSL::PKey::RSA.new(str);
-      elsif  Rails.env.development?
-        rsa_private = OpenSSL::PKey::RSA.new(File.read(Rails.root.join('auth/service.key')))
-      end
+    #   if Rails.env.production?
+    #     tmp = ENV['SERVICE_KEY_F'] + ENV['SERVICE_KEY_L']
+    #     str = tmp.gsub(/\\n/, "\n")
+    #     rsa_private = OpenSSL::PKey::RSA.new(str);
+    #   elsif  Rails.env.development?
+    #     rsa_private = OpenSSL::PKey::RSA.new(File.read(Rails.root.join('auth/service.key')))
+    #   end
       
-      token = JWT.encode(payload, rsa_private, "RS256")
+    #   token = JWT.encode(payload, rsa_private, "RS256")
 
       # JWTを末尾につけたURLを生成
-      url = ENV["EMAIL_AUTH_URL"] + "/" + token if token
+    #   url = ENV["EMAIL_AUTH_URL"] + "/" + token if token
+
+      url = "testdatestda"
       
       # urlを記載したメールをフォームからpostされたメールアドレスに送信
       UserMailer.send_auth_email(params[:name], params[:email], url).deliver_now
