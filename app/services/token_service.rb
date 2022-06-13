a275a35354f738d946a79c9ec79198179f220e5f
yuichi04 <sparklingengineer.yuya04gen@gmail.com>
2022-06-13 17:44:58 +0900

    Improve support for production environments

--- app/services/token_service.rb
+++ app/services/token_service.rb
@@ -10,7 +10,15 @@ class TokenService
             sub: id,
             exp: (DateTime.current + 14.days).to_i
         }
-        rsa_private = OpenSSL::PKey::RSA.new(File.read(Rails.root.join('auth/service.key')))
+
+        # 秘密鍵の取得
+        if Rails.env.production?
+            tmp = ENV['SERVICE_KEY_F'] + ENV['SERVICE_KEY_L']
+            str = tmp.gsub(/\\n/, "\n")
+            rsa_private = OpenSSL::PKey::RSA.new(str);
+        elsif Rails.env.development?
+            rsa_private = OpenSSL::PKey::RSA.new(File.read(Rails.root.join('auth/service.key')))
+        end
         JWT.encode(payload, rsa_private, "RS256")
     end
 end
